07ce92f9f7f2e091ba0eb07ac45ce891835d3c37
artahadhahd <artaazadegan1@gmail.com>
2023-10-29 21:00:33 +0100

    minor bug fixes

--- include/strange.h
+++ include/strange.h
@@ -1,17 +1,18 @@
-// TODO: Optimizations
+/* TODO: Optimizations */
 #ifndef STRANGE_H
 #define STRANGE_H 0.11f
+#ifdef __cplusplus
+#include <cstdlib>
+#include <cstring>
+#else
+#include <stdlib.h> /* malloc realloc */
+#include <string.h> /* strcat strlen */
+#endif
 #ifndef _WIN32
 #define STRAPI __attribute__((visibility("default")))
 #else
 #define STRAPI __attribute__((dllexport))
 #endif
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <stdlib.h> // malloc realloc
-#include <string.h> // strcat strlen
 
 #define EXTENDFACTOR 1.5
 
@@ -25,7 +26,7 @@ typedef enum StrangeErrorHandler StrangeError;
 struct string {
   char * content;
   size_t capacity, length; 
-#ifdef __cplusplus // Destructor for C++
+#ifdef __cplusplus
   compl string() {
     free(this->content);
   }
@@ -34,19 +35,22 @@ struct string {
 
 typedef struct string String;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 STRAPI String string_new();
 STRAPI String string_from(const char * input);
 STRAPI StrangeError string_append_str(const char * source, String * dest);
 STRAPI StrangeError string_append_char(const char c, String * dest);
 STRAPI void string_free(const String * string);
 
-// C11 and above
+#ifdef __cplusplus
+}
+#endif /* extern "C" */
+/* C11 and above */
 #if __STDC_VERSION__ >= 201112L
 #define string_append(src, dest) _Generic((src), char : string_append_char, char * : string_append_str, const char * : string_append_str)(src, dest)
 #endif
 
-#ifdef __cplusplus
-}
-#endif // extern "C"
-
-#endif // STRANGE_H
+#endif /* STRANGE_H */
